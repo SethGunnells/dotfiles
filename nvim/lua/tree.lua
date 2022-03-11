af95af6e6dbca662d4d0662498302096b09ebb35
Seth Gunnells <sethgunnells@gmail.com>
2022-03-10 23:18:34 -0600

    Simplify Nvim-Tree config

--- nvim/lua/tree.lua
+++ nvim/lua/tree.lua
@@ -1,18 +1,3 @@
-treeView = {}
-function treeView.open() 
-  require'bufferline.state'.set_offset(31, 'FileTree')
-  require'nvim-tree'.find_file(true)
-end
-function treeView.close()
-  require'bufferline.state'.set_offset(0)
-  require'nvim-tree'.close()
-end
-function treeView.toggle()
-  if require'nvim-tree.view'.win_open() then
-    treeView.close()
-  else
-    treeView.open()
-  end
-end
-vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>lua treeView.toggle()<CR>', { noremap = true })
+require'nvim-tree'.setup({ view = { width = '20%' } })
+vim.api.nvim_set_keymap('n', '<leader>t', '<cmd>NvimTreeToggle<CR>', { noremap = true })
 
