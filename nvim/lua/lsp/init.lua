10c12100a8ee4c8684d576a467d6ce978c35e2e2
Seth Gunnells <sethgunnells@gmail.com>
2023-02-07 10:36:43 -0600

    Add shortcut for code actions

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,7 +1,6 @@
 local map = vim.keymap.set
 map('n', '<leader>h', vim.lsp.buf.hover)
-map('n', '<leader>a', vim.lsp.buf.code_action)
-map('x', '<leader>a', vim.lsp.buf.range_code_action)
+map({'n', 'x'}, '<leader>a', vim.lsp.buf.code_action)
 
 require'lsp.diagnostics'
 require'lsp.formatting'.setupFormatOnSave()
