5040a30aa80d9de5a52e09fedbe781403baea8fb
Seth Gunnells <sethgunnells@hey.com>
2023-11-03 23:30:02 -0500

    Add lots of LSP support for langs

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,11 +1,15 @@
 local map = vim.keymap.set
 map('n', '<leader>h', vim.lsp.buf.hover)
-
-require'lsp.diagnostics'
-require'lsp.formatting'.setupFormatOnSave()
-require'lsp.tsserver'
-require'lsp.lua'
-require'lsp.null-ls'
 map({ 'n', 'x' }, '<leader>a', vim.lsp.buf.code_action)
 map('n', '<leader><leader>r', vim.lsp.buf.rename)
 
+require 'lsp.diagnostics'
+require 'lsp.formatting'.setupFormatOnSave()
+require 'lsp.tsserver'
+require 'lsp.lua'
+require 'lsp.null-ls'
+require 'lsp.vlang'
+require 'lsp.gleam'
+require 'lsp.rust'
+require 'lsp.julia'
+require 'lsp.unison'
